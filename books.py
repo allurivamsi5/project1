4ae7ae8b98a168811d1437473984827d2b4290df
allurivamsi5 <allurivamsi5@msitprogram.net>
2020-04-21 00:18:50 +0530

    Import Books

--- books.py
+++ books.py
@@ -1,3 +1,23 @@
-import requests
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "1cdsYpCiJTacj1y2EgwYFg", "isbns": "9781632168146"})
-print(res.json())
+import os, csv
+from books_database import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    Base.metadata.create_all(bind=engine)
+    f = open("books.csv")
+    reader = csv.reader(f)
+    next(reader)
+    for isbn, title, author, year in reader:
+        book = Books(isbn=isbn, title=title, author=author, year=int(year))
+        db.add(book)
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    main()
